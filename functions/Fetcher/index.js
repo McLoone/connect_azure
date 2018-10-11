9115b410c227d15fc5f79df22d70dd5a162c2580
P-A Söderqvist <pa.soderqvist@gmail.com>
2018-10-12 00:31:17 +0200

    Update function

--- functions/Fetcher/index.js
+++ functions/Fetcher/index.js
@@ -2,8 +2,10 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
+        context.log('Event Hub message created at: ', timeStamp); 
+        context.bindings.outputEventHubMessage = req.body.name;
+        
+        context.res = {            
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
